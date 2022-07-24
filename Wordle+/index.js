5177dc23a9ba68eaecca8e766d799a3fcce3fa5a
pramsuiitd <97740881+pramsuiitd@users.noreply.github.com>
2022-07-24 16:06:54 +0530

    Uploading Worlde+ file

--- Wordle+/index.js
+++ Wordle+/index.js
@@ -1,13 +1,22 @@
 const http = require("http");
 
-const SECRET = "CIGAR"; // You can set any word as the secret answer
+const SECRET = "HELLO"; // You can set any word as the secret answer
 
 function myFunction(req, res) {
 	// console.log({req}); // You can uncomment this to see the request object
-	console.log(req.url);
-
-	const GUESS = ""; // Write logic to parse the word which the user guessed from the URL string
-	const feedback = ""; // Write logic to compare the word with the secret, and generate the feedback string
+	const req_url = req.url;
+	const string_url = String(req_url);
+	let GUESS = string_url.substring(string_url.indexOf('=')+1); // Write logic to parse the word which the user guessed from the URL string
+	GUESS = GUESS.replace("/favicon.ico","");
+	let feedback = ""; // Write logic to compare the word with the secret, and generate the feedback string
+	for(let i = 0; i < 5; i++){
+	if(GUESS[i] == SECRET[i])
+		feedback += "g";
+	else if(SECRET.includes(GUESS[i]))
+		feedback += "y";
+	else
+		feedback += "b";
+	}
 
 	res.write(feedback);
 	res.end();
